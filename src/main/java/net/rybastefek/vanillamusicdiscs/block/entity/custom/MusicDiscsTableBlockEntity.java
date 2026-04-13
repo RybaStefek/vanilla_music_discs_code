d8682c1bc1f546e911bbdc7ceaadd0f8b3f9c582
RybaStefek <rybapan0@gmail.com>
2026-04-13 21:33:16 +0200

    Commit message

--- src/main/java/net/rybastefek/vanillamusicdiscs/block/entity/custom/MusicDiscsTableBlockEntity.java
+++ src/main/java/net/rybastefek/vanillamusicdiscs/block/entity/custom/MusicDiscsTableBlockEntity.java
@@ -5,6 +5,7 @@ import net.minecraft.core.Direction;
 import net.minecraft.nbt.CompoundTag;
 import net.minecraft.network.chat.Component;
 import net.minecraft.network.chat.TextComponent;
+import net.minecraft.network.chat.TranslatableComponent;
 import net.minecraft.world.Containers;
 import net.minecraft.world.MenuProvider;
 import net.minecraft.world.SimpleContainer;
@@ -117,7 +118,7 @@ public class MusicDiscsTableBlockEntity extends BlockEntity implements MenuProvi
 
     @Override
     public Component getDisplayName() {
-        return new TextComponent("Music Discs Table");
+        return new TranslatableComponent("container.vanillamusicdiscs.music_discs_table");
     }
 
     @Nullable
